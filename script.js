04cf5f75c3b31c20fff62065fa73eeb16a142f76
wahyu12v <mkzcmd@gmail.com>
2024-02-25 17:35:07 +0700

    memperbarui tampilan dan responsif

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('.categories a').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
